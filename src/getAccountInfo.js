7f11f8290673bbd96a6062336eaebb97cb381627
Tomoyuki Izumi <tomo15800@gmail.com>
2017-12-14 23:15:46 +0900

    円転評価額取得ロジックを実装

---
+++ src/getAccountInfo.js
@@ -0,0 +1,42 @@
+import zaif from "zaif.jp"
+import _ from "lodash"
+
+const apiKey = process.env.ZAIF_API_KEY
+const apiSecret = process.env.ZAIF_API_SECRET
+
+const privateApi = zaif.createPrivateApi(apiKey, apiSecret, 'node-zaif')
+
+const getAccountInfo = privateApi.getInfo().then((accountInfo) => {
+  const requestList = _.map(accountInfo.deposit, (value, ccy) => {
+    // 円の場合はそのまま返す
+    if (ccy === "jpy") {
+      return {
+        ccy,
+        value
+      }
+    }
+    // 数量0はnull
+    if (value === 0) {
+      return
+    }
+    // 円転評価額を算出
+    return api.ticker(ccy + "_jpy").then((ticker) => {
+        return {
+          ccy,
+          value: ticker.bid * value
+        }
+    })
+  })
+
+  return Promise.all(requestList).then((allResult) => {
+    const results = {}
+    allResult.forEach((values) => {
+      if (values != null) {
+        results[values.ccy] = values.value
+      }
+    })
+    return results
+  })
+})
+
+export default getAccountInfo
